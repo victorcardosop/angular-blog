8a209e974d94c1fcff35791682df71ed58f55fee
Victor Pimenta <victorcpimenta@hotmail.com>
2023-02-10 20:36:06 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "NOVO HOMEM DE FERRO EM 3D",
+        "description": "Marvel studios anuncia novo filme do Homem de Ferro.",
+        "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    },
+    {
+        "id":"2",
+        "title": "Nova série anunciada no Disney+",
+        "description": "Uma nova série foi anunciada para o Disney+",
+        "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    },
+    {
+        "id":"3",
+        "title": "Novo filme do Pantera Negra lançado em breve",
+        "description": "Teremos um novo filme do Pantera Negra, em breve nos cinemas.",
+        "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/F6482D58C8BB4D899FA4682BF27652C3E1BB73F5971E5F963994112F26A73690/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    },
+    {
+        "id":"4",
+        "title": "Novo filme dos X-men está em pós produção, afirma diretor",
+        "description": "Diretor afirma que uma sequência para os X-men está próxima.",
+        "photoCover":"https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
+    },
+]
